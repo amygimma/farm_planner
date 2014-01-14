9ef58cdb93c033e224b73463efaf951af8e9baaf
amyg225 <amyg225@gmail.com>
2014-01-14 13:48:48 -0500

    Initialize repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FarmPlanner::Application.config.secret_key_base = '85498fef601bed4b031b696ddd78e79594a7f37809fc2faf19cf6e82081b637b50f6cb1c97b6b4b6a69b9f7e44b5c7641e7ab0058d52af4172358080235dcafb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FarmPlanner::Application.config.session_store :cookie_store, key: '_farm_planner_session'
