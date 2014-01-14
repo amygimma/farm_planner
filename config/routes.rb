1fa4f51a61b09381f3e985111a68142d9abaf938
amyg225 <amyg225@gmail.com>
2014-01-14 15:47:12 -0500

    Add layout html and home page, update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 FarmPlanner::Application.routes.draw do
+  root "static_pages#home"
+  match '/about',   to: "static_pages#about",    via: 'get'
+  match '/help',    to: "static_pages#help",     via: 'get'
+  match '/contact', to: "static_pages#contact",  via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
